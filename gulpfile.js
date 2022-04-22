19f7d7a33b4aef988c771f9dca1b136f00fafe31
Pyotr Ryzhov <maestro_pete@inbox.ru>
2022-04-22 15:44:16 +0300

    added npm glide.js and normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict';
 
-const { series, parallel, src, dest, watch } = require("gulp");
-const plumber = require("gulp-plumber");
-const sass = require("gulp-sass")(require("sass"));
-const sourcemaps = require("gulp-sourcemaps");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const postcssAnimation = require("postcss-animation");
-const postcssFlexbugs = require("postcss-flexbugs-fixes");
-const browserSync = require("browser-sync").create();
-const del = require("del");
-const babel = require("gulp-babel");
-const uglify = require("gulp-uglify");
-const concat = require("gulp-concat");
-const pug = require("gulp-pug");
-const prettyHtml = require("gulp-pretty-html");
-const replace = require("gulp-replace");
-const ghPages = require("gh-pages");
-const path = require("path");
-const cpy = require("cpy");
-const imagemin = require("gulp-imagemin");
-const spritesmith = require("gulp.spritesmith");
-const merge = require("merge-stream");
-const buffer = require("vinyl-buffer");
+const { series, parallel, src, dest, watch } = require('gulp');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const postcssAnimation = require('postcss-animation');
+const postcssFlexbugs = require('postcss-flexbugs-fixes');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const pug = require('gulp-pug');
+const prettyHtml = require('gulp-pretty-html');
+const replace = require('gulp-replace');
+const ghPages = require('gh-pages');
+const path = require('path');
+const cpy = require('cpy');
+const imagemin = require('gulp-imagemin');
+const spritesmith = require('gulp.spritesmith');
+const merge = require('merge-stream');
+const buffer = require('vinyl-buffer');
 
 const nth = {};
-nth.config = require("./config.js");
+nth.config = require('./config.js');
 
 const dir = nth.config.dir;
 const options = nth.config.options;
@@ -61,42 +61,42 @@ exports.copyAssets = copyAssets;
 
 // отправка папки build на gh-pages
 function deploy(cb) {
-  ghPages.publish(path.join(process.cwd(), "./build"), cb);
+  ghPages.publish(path.join(process.cwd(), './build'), cb);
 }
 exports.deploy = deploy;
 
 function compilePug() {
-  return src(dir.src + "pages/**/*.pug")
+  return src(dir.src + 'pages/**/*.pug')
     .pipe(
       plumber({
         errorHandler: function (err) {
           console.log(err.message);
-          this.emit("end");
-        },
+          this.emit('end');
+        }
       })
     )
     .pipe(pug())
     .pipe(
       prettyHtml({
         indent_size: 2,
-        indent_char: " ",
-        unformatted: ["code", "em", "strong", "span", "i", "b", "br"],
-        content_unformatted: [],
+        indent_char: ' ',
+        unformatted: ['code', 'em', 'strong', 'span', 'i', 'b', 'br'],
+        content_unformatted: []
       })
     )
     .pipe(
-      replace(/^(\s*)(<button.+?>)(.*)(<\/button>)/gm, "$1$2\n$1  $3\n$1$4")
+      replace(/^(\s*)(<button.+?>)(.*)(<\/button>)/gm, '$1$2\n$1  $3\n$1$4')
     )
     .pipe(
       replace(
         /^( *)(<.+?>)(<script>)([\s\S]*)(<\/script>)/gm,
-        "$1$2\n$1$3\n$4\n$1$5\n"
+        '$1$2\n$1$3\n$4\n$1$5\n'
       )
     )
     .pipe(
       replace(
         /^( *)(<.+?>)(<script\s+src.+>)(?:[\s\S]*)(<\/script>)/gm,
-        "$1$2\n$1$3$4"
+        '$1$2\n$1$3$4'
       )
     )
     .pipe(dest(dir.build));
@@ -104,13 +104,13 @@ function compilePug() {
 exports.compilePug = compilePug;
 
 function compileStyles() {
-  return src(dir.src + "scss/style.scss")
+  return src(dir.src + 'scss/style.scss')
     .pipe(
       plumber({
         errorHandler: function (err) {
           console.log(err.message);
-          this.emit("end");
-        },
+          this.emit('end');
+        }
       })
     )
     .pipe(sourcemaps.init())
@@ -119,72 +119,71 @@ function compileStyles() {
       postcss([
         postcssAnimation(),
         postcssFlexbugs(),
-        autoprefixer({ overrideBrowserslist: ["last 2 version"] }),
+        autoprefixer({ overrideBrowserslist: ['last 2 version'] })
       ])
     )
-    .pipe(sourcemaps.write("/"))
-    .pipe(dest(dir.build + "css/"))
+    .pipe(sourcemaps.write('/'))
+    .pipe(dest(dir.build + 'css/'))
     .pipe(browserSync.stream());
 }
 exports.compileStyles = compileStyles;
 
 function processJs(cb) {
   if (options.processJs)
-    return src(dir.src + "js/script.js")
+    return src(dir.src + 'js/script.js')
       .pipe(
         plumber({
           errorHandler: function (err) {
             console.log(err.message);
-            this.emit("end");
-          },
+            this.emit('end');
+          }
         })
       )
       .pipe(
         babel({
-          presets: ["@babel/env"],
+          presets: ['@babel/env']
         })
       )
       .pipe(uglify())
-      .pipe(concat("script.min.js"))
-      .pipe(dest(dir.build + "js/"));
-    else {
-      cb();
-    }
+      .pipe(concat('script.min.js'))
+      .pipe(dest(dir.build + 'js/'));
+  else {
+    cb();
+  }
 }
 exports.processJs = processJs;
 
 function copyJsVendors(cb) {
   if (options.copyJsVendors) {
-    return src(["node_modules/svg4everybody/dist/svg4everybody.min.js"])
-      .pipe(concat("vendors.min.js"))
-      .pipe(dest(dir.build + "js/"));
-  }
-  else {
+    return src([
+      'node_modules/simplestatemanager/dist/ssm.min.js',
+      'node_modules/@glidejs/glide/dist/glide.js',
+    ])
+      .pipe(concat('vendors.min.js'))
+      .pipe(dest(dir.build + 'js/'));
+  } else {
     cb();
-  };
+  }
 }
 
 function copyImages() {
   return src([
-    dir.src + "img/**/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}",
-    "!"+dir.src+"img/spritesmith/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}"
-  ]
-    ).pipe(
-    dest(dir.build + "img/")
-  );
+    dir.src + 'img/**/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}',
+    '!' + dir.src + 'img/spritesmith/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}'
+  ]).pipe(dest(dir.build + 'img/'));
 }
 exports.copyImages = copyImages;
 
 function copyVideo() {
-  return src(dir.src + "video/**/*{.mp4,.avi,.webm}").pipe(
-    dest(dir.build + "video/")
+  return src(dir.src + 'video/**/*{.mp4,.avi,.webm}').pipe(
+    dest(dir.build + 'video/')
   );
 }
 exports.copyVideo = copyVideo;
 
 function copyFonts() {
-  return src(dir.src + "fonts/**/*.{ttf,eot,svg,woff,woff2}").pipe(
-    dest(dir.build + "fonts/")
+  return src(dir.src + 'fonts/**/*.{ttf,eot,svg,woff,woff2}').pipe(
+    dest(dir.build + 'fonts/')
   );
 }
 exports.copyFonts = copyFonts;
@@ -193,13 +192,13 @@ exports.copyFonts = copyFonts;
 // создание png-спрайта
 function generatePngSprite(cb) {
   let spritePngPath = `${dir.src}img/spritesmith/`;
-  if (!fileExist(spritePngPath + "*.{jpg,jpeg,png,webp,gif}")) {
+  if (!fileExist(spritePngPath + '*.{jpg,jpeg,png,webp,gif}')) {
     let spriteData = src(`${spritePngPath}*.{jpg,jpeg,png,webp,gif}`).pipe(
       spritesmith({
-        imgName: "sprite-png.png", // название собраного спрайта
-        cssName: "_sprite-png.scss", // название css файла
+        imgName: 'sprite-png.png', // название собраного спрайта
+        cssName: '_sprite-png.scss', // название css файла
         padding: 4,
-        imgPath: "../img/sprite-png.png",
+        imgPath: '../img/sprite-png.png'
       })
     );
     let styleStream = spriteData.css.pipe(dest(`${dir.src}scss/`));
@@ -226,28 +225,34 @@ exports.clean = clean;
 function serve() {
   browserSync.init({
     server: dir.build,
-    startPath: "index.html",
+    startPath: 'index.html',
     open: false,
-    port: 8080,
+    port: 8080
   });
   watch(
-    [dir.src + "scss/*.scss", dir.src + "scss/blocks/*.scss"],
+    [dir.src + 'scss/*.scss', dir.src + 'scss/blocks/*.scss'],
     compileStyles
   );
-  watch([dir.src + "pages/*.pug", dir.src + "pug/**/*.pug"], compilePug);
-  watch(dir.src + "js/**/*.js", processJs);
-  watch(dir.src + "img/spritesmith/*.{jpg,jpeg,png,webp,gif}", generatePngSprite);
-  watch([
-    dir.src + "img/**/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}",
-    "!"+dir.src+"img/spritesmith/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}"
-  ],
+  watch([dir.src + 'pages/*.pug', dir.src + 'pug/**/*.pug'], compilePug);
+  watch(dir.src + 'js/**/*.js', processJs);
+  watch(
+    dir.src + 'img/spritesmith/*.{jpg,jpeg,png,webp,gif}',
+    generatePngSprite
+  );
+  watch(
+    [
+      dir.src + 'img/**/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}',
+      '!' +
+        dir.src +
+        'img/spritesmith/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}'
+    ],
     copyImages
   );
   watch([
-    dir.build + "*.html",
-    dir.build + "js/*.js",
-    dir.build + "img/**/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}",
-  ]).on("change", browserSync.reload);
+    dir.build + '*.html',
+    dir.build + 'js/*.js',
+    dir.build + 'img/**/*.{jpg,jpeg,png,svg,webp,gif,webmanifest}'
+  ]).on('change', browserSync.reload);
 }
 
 exports.build = series(
@@ -261,7 +266,7 @@ exports.build = series(
     copyImages,
     copyVideo,
     copyFonts,
-    copyAssets,
+    copyAssets
   )
 );
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,52 @@
 function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+  if (
+    document.attachEvent
+      ? document.readyState === 'complete'
+      : document.readyState !== 'loading'
+  ) {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
 
-ready(function(){
-  console.log('DOM ready');
+ready(function () {
+  const GlideOptions = {
+    type: 'carousel',
+    startAt: 0,
+    perView: 4,
+    gap: 50,
+    animationDuration: 700,
+
+    breakpoints: {
+      1440: {
+        perView: 4,
+      },
+      1200: {
+        perView: 3,
+      },
+      1024: {
+        perView: 2,
+        gap: 30,
+      },
+      768: {
+        perView: 1,
+        gap: 20,
+      }
+    }
+  };
+
+  let GlideGallery;
+
+  ssm.addState({
+    id: 'mobile',
+    query: '(max-width: 1024px)',
+    onEnter: function () {
+      GlideGallery = new Glide(document.querySelector('#glide'), GlideOptions);
+      GlideGallery.mount();
+    },
+    onLeave: function () {
+      GlideGallery.destroy();
+    }
+  })
 });
